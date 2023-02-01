891031d3e9c2c95e04da0380b10f810b62cb76a5
Manoj Kumawat <imkumawatmanoj@gmail.com>
2023-02-01 09:21:20 +0530

    cx

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const port = 4000;
 app.get("/", (req, res) => {
   res.send("Hello Mr. Manoj Kumawat......!");
 });
+
+app.get("/myhealth", (req, res) => {
+  console.log("Endpoint: ", req.url);
+  res.send("ok");
+});
 console.log("New update");
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
